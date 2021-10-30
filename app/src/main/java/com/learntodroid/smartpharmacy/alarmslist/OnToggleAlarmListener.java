4e63153caf7f8f0fa5a011be12474ba3c60e7bb8
Victor Azevedo <victor.sil.azevedo@gmail.com>
2021-10-30 10:55:48 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/learntodroid/smartpharmacy/alarmslist/OnToggleAlarmListener.java
@@ -0,0 +1,7 @@
+package com.learntodroid.smartpharmacy.alarmslist;
+
+import com.learntodroid.smartpharmacy.data.Alarm;
+
+public interface OnToggleAlarmListener {
+    void onToggle(Alarm alarm);
+}
